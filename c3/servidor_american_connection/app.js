c74dd1a512cbde76eaced7af221ae9baaf09b40d
DavidNavarrete07 <davidalejandro20-09@outlook.es>
2022-07-28 14:05:43 -0400

    Terminando ejercicio asincronico de la clase3 sin ajax

--- c3/servidor_american_connection/app.js
+++ c3/servidor_american_connection/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const fs = require('fs');
+const moment = require('moment');
+moment().locale('es').format('l');
 const app = express();
 
 const hostname = '127.0.0.1'
@@ -9,40 +11,130 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.get('/add', (req, res) => {
-    if (req.query.fileName.length != 0 || req.query.contentFile.length != 0) {
-        fs.writeFile('public/documents/' + req.query.fileName.trim(), req.query.contentFile.trim(), 'utf-8', function () {
-            res.send("Archivo creado con éxito");
+    if (req.query.fileName != null || req.query.contentFile != null) {
+        fs.writeFile('public/documents/' + req.query.fileName.trim() + '.txt', `${getDate()} \n${req.query.contentFile.trim()}`, 'utf-8', function () {
+            res.send(
+                `<html>
+                    <h2>Archivo creado con éxito</h2>
+                    <h3>El archivo ${req.query.fileName.trim()}.txt se creó con éxito</h3>
+                    <button onclick="window.location.href='/index.html'">Volver</button>
+                </html>`
+            );
         });
-    }else{
-        res.send("Ingrese los datos correspondientes");
+    } else {
+        res.send(
+            `<html>
+                <h2>Ingrese los datos correspondientes</h2>
+                <button onclick="window.location.href='/index.html'">Volver</button>
+            </html>`
+        );
     }
 });
 
 app.get('/read', (req, res) => {
-    if (fs.existsSync('public/documents/' + req.query.fileNameRead.trim())) {
-        fs.readFile('public/documents/' + req.query.fileNameRead.trim(), function (err, data) {
-            res.send(data);
-        });
+    if (req.query.fileNameRead != null) {
+        if (fs.existsSync('public/documents/' + req.query.fileNameRead.trim() + '.txt')) {
+            fs.readFile('public/documents/' + req.query.fileNameRead.trim() + '.txt', 'utf-8', function (err, data) {
+                let arr = data.trim().split("\n").join("<br>");
+                res.send(
+                    `<html>
+                        <h4>Archivo: ${req.query.fileNameRead.trim()}.txt</h4>
+                        <h4>Contenido del archivo: </h4>
+                            <p>${arr}</p>
+                        <button onclick="window.location.href='/index.html'">Volver</button>
+                    </html>`
+                );
+            });
+        } else {
+            res.send(
+                `<html>
+                    <h2>El archivo no existe</h2>
+                    <button onclick="window.location.href='/index.html'">Volver</button>
+                </html>`
+            );
+        }
     } else {
-        res.send("Este archivo no existe");
+        res.send(
+            `<html>
+                <h2>Ingrese los datos correspondientes</h2>
+                <button onclick="window.location.href='/index.html'">Volver</button>
+            </html>`
+        );
     }
 });
 
 app.get('/rename', (req, res) => {
-    fs.rename('public/documents/'+req.query.fileNameRename.trim(), 'public/documents/'+req.query.fileNameNew.trim(), function(){
-        res.send("Archivo renombrado");
-    });
+    if (req.query.fileNameRename != null || req.query.fileNameNew != null) {
+        if (fs.existsSync('public/documents/' + req.query.fileNameRename.trim() + '.txt')) {
+            fs.rename('public/documents/' + req.query.fileNameRename.trim() + '.txt', 'public/documents/' + req.query.fileNameNew.trim() + '.txt', function () {
+                res.send(
+                    `<html>
+                        <h2>Archivo renombrado con éxito</h2>
+                        <h3>El archivo ${req.query.fileNameRename.trim()}.txt se renombró con éxito
+                         y su nuevo nombre es ${req.query.fileNameNew.trim()}.txt
+                        </h3>
+                        <button onclick="window.location.href='/index.html'">Volver</button>
+                    </html>`
+                );
+            });
+        } else {
+            res.send(
+                `<html>
+                    <h2>El archivo no existe</h2>
+                    <button onclick="window.location.href='/index.html'">Volver</button>
+                </html>`
+            );
+        }
+    } else {
+        res.send(
+            `<html>
+                <h2>Ingrese los datos correspondientes</h2>
+                <button onclick="window.location.href='/index.html'">Volver</button>
+            </html>`
+        );
+    }
+});
+
+app.get('/confirm_delete', (req, res) => {
+    res.send(
+        `<html>
+            <h4>Archivo eliminado: ${req.query.fileNameDelete.trim()}.txt</h4>
+            <button onclick="window.location.href='/index.html'">Volver</button>
+        </html>`
+    );
 });
 
 app.get('/delete', (req, res) => {
-    if(fs.existsSync('public/documents/'+req.query.fileNameDelete.trim())){
-        fs.unlink('public/documents/' + req.query.fileNameDelete.trim(), function(){
-            res.send("Archivo eliminado con éxito");
-        });
-    }else{
-        res.send("El archivo no existe");
+    if (req.query.fileNameDelete != null) {
+        if (fs.existsSync('public/documents/' + req.query.fileNameDelete.trim() + '.txt')) {
+            fs.unlink('public/documents/' + req.query.fileNameDelete.trim() + '.txt', function () {
+                res.send(
+                    `<p>Tu Tu solicitud para eliminar el archivo ${req.query.fileNameDelete.trim()+'.txt'} se está procesando
+                     <script>
+                        setTimeout(function(){
+                            window.location = '/confirm_delete?fileNameDelete=${req.query.fileNameDelete.trim()}'
+                        }, 3000);
+                     </script>
+                    `);
+            });
+        } else {
+            res.send(
+                `<html>
+                    <h2>El archivo no existe</h2>
+                    <button onclick="window.location.href='/index.html'">Volver</button>
+                </html>`
+            );
+        }
+    } else {
+        res.send(
+            `<html>
+                <h2>Ingrese los datos correspondientes</h2>
+                <button onclick="window.location.href='/index.html'">Volver</button>
+            </html>`
+        );
     }
 });
+
 
 app.get('*', (req, res) => {
     res.send("Ruta no implementada");
@@ -51,3 +143,14 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
     console.log(`Servidor corriendo en http://${hostname}:${port}/`);
 });
+
+function getDate() {
+    let day = moment().date();
+    let month = moment().month() + 1;
+    let year = moment().year();
+    let date;
+    (day < 10) ? day = '0' + day : day;
+    (month < 10) ? month = '0' + month : month;
+    date = `${day}/${month}/${year}`;
+    return date;
+}
