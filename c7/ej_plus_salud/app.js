6a8b71ef91def77f708786ff4e743d2b85915c38
DavidNavarrete07 <davidalejandro20-09@outlook.es>
2022-08-04 10:48:01 -0400

    Trabajando con persistencia en un archivo JSON

--- c7/ej_plus_salud/app.js
+++ c7/ej_plus_salud/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const chalk = require('chalk');
 const axios = require('axios');
+const fs = require('fs').promises;
 const { v4: uuidv4 } = require('uuid');
 const moment = require('moment');
 moment().locale('es').format('l');
@@ -10,30 +11,45 @@ app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const DB = {
+    async createUser(newUser) {
+        let fileDB = await fs.readFile('db/db.json', 'utf-8');
+        fileDB = JSON.parse(fileDB);
+        fileDB.users.push(newUser);
+        fileDB = JSON.stringify(fileDB);
+        await fs.writeFile('db/db.json', fileDB, 'utf-8');
+    },
+    async getUsers() {
+        let fileDB = await fs.readFile('db/db.json', 'utf-8');
+        fileDB = JSON.parse(fileDB);
+        return fileDB.users;
+    }
+}
+
 // Rutas
-const users = [];
-app.get('/users', (req, res) => {
-    for (let user of users) {
+app.get('/users', async (req, res) => {
+    for (let user of await DB.getUsers()) {
         console.log(chalk.underline.bold.bgWhite.blue(`Usuario: ${user.id} - ${user.firstName} ${user.lastName} - ${user.email} - ${user.timestamp}`));
     }
-    res.json({users});
+    const users = await DB.getUsers()
+    res.json({ users });
 });
 
 app.get('/new-user', async (req, res) => {
-        try {
-            const res_api = await axios.get('https://randomuser.me/api');
-            let newUser = {
-                id: uuidv4(),
-                firstName: res_api.data.results[0].name.first,
-                lastName: res_api.data.results[0].name.last,
-                email: res_api.data.results[0].email,
-                timestamp: moment().format('MMMM Do YYYY, h:mm:ss')
-            }
-            users.push(newUser);
-            res.redirect('/');
-        } catch (error) {
-            console.log(error);
+    try {
+        const res_api = await axios.get('https://randomuser.me/api');
+        let newUser = {
+            id: uuidv4(),
+            firstName: res_api.data.results[0].name.first,
+            lastName: res_api.data.results[0].name.last,
+            email: res_api.data.results[0].email,
+            timestamp: moment().format('MMMM Do YYYY, h:mm:ss')
         }
+        await DB.createUser(newUser);
+        res.redirect('/');
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 app.get('*', (req, res) => {
